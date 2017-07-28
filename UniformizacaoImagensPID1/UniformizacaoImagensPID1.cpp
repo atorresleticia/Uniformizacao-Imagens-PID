60f7c708e8921c9e38fed24c53a687545d77bd39
Letícia Torres <atorresleticia@gmail.com>
2017-07-27 22:51:52 -0300

    Média e variância calculadas

--- UniformizacaoImagensPID1/UniformizacaoImagensPID1.cpp
+++ UniformizacaoImagensPID1/UniformizacaoImagensPID1.cpp
@@ -13,8 +13,8 @@
 #include "stdafx.h"
 #include "bitmap.h"
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -33,27 +33,25 @@ void ler_bitmap(FILE* img_ref, FILE* img_ajuste)
 	fread(&img_ref_info, sizeof(BitMapInfoHeader), 1, img_ref);
 	fread(&img_ajuste_info, sizeof(BitMapInfoHeader), 1, img_ajuste);
 
-	FILE* ref = fopen("ref.txt", "w");
-	FILE* ajuste = fopen("ajuste.txt", "w");
+	FILE* tes = fopen("tes.txt", "w");
+	FILE* tese = fopen("tese.txt", "w");
 
 	while (fread(&pix, sizeof(Pixel), 1, img_ref))
 	{
 		bitmap_ref.push_back(pix);
-		fprintf(ref, "%d %d %d\n", pix.B, pix.G, pix.R);
+		fprintf(tes, "%d %d %d\n", pix.B, pix.G, pix.R);
 	}
 
 	while (fread(&pix, sizeof(Pixel), 1, img_ajuste))
 	{
 		bitmap_ajuste.push_back(pix);
-		fprintf(ajuste, "%d %d %d\n", pix.B, pix.G, pix.R);
+		fprintf(tese, "%d %d %d\n", pix.B, pix.G, pix.R);
 	}
 }
 
 int* calcula_media(vector<Pixel> bitmaps)
 {
-	cout << bitmaps.size() << endl;
 	int mediaRGB[3] = { 0 };
-
 
 	for (Pixel pix : bitmaps)
 	{
@@ -61,6 +59,7 @@ int* calcula_media(vector<Pixel> bitmaps)
 		mediaRGB[1] += pix.G;
 		mediaRGB[2] += pix.R;
 	}
+
 	mediaRGB[0] /= bitmaps.size();
 	mediaRGB[1] /= bitmaps.size();
 	mediaRGB[2] /= bitmaps.size();
@@ -85,18 +84,31 @@ int* calcula_variancia(vector<Pixel> bitmaps, int* media_img)
 
 	return varianciaRGB;
 }
+
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		cout << "Argumentos = <nome_programa> <imagem_referencia> <imagem_ajuste> " << endl;
+		cout << endl << "Argumentos = <nome_programa> <imagem_referencia> <imagem_ajuste> " << endl;
 		exit(1);
 	}
 
 	FILE* img_ref = fopen(argv[1], "rb");
 	FILE* img_ajuste = fopen(argv[2], "rb");
 
+	if (!img_ref)
+	{
+		cout << "Erro ao abrir a imagem referência" << endl;
+		exit(1);
+	}
+	if (!img_ajuste)
+	{
+		cout << "Erro ao abrir a imagem ajuste" << endl;
+		exit(1);		
+	}
+
 	ler_bitmap(img_ref, img_ajuste);
+
 	int* mr = calcula_media(bitmap_ref);
 	int* ma = calcula_media(bitmap_ajuste);
 	int* Vr = calcula_variancia(bitmap_ref, mr);
